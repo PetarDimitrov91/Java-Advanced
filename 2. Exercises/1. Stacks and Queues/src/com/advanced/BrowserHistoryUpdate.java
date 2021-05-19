e7fb1ef8a9e13ff7fe5fd05c955aafb3ac98b3dc
Petar Dimitrov <79804094+PetarDimitrov91@users.noreply.github.com>
2021-05-19 12:30:37 +0200

    Update last task

---
+++ 2. Exercises/1. Stacks and Queues/src/com/advanced/BrowserHistoryUpdate.java
@@ -0,0 +1,35 @@
+package com.advanced;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BrowserHistoryUpdate {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		ArrayDeque<String> browser = new ArrayDeque<>();
+		ArrayDeque<String> forward = new ArrayDeque<>();
+
+		String line = console.nextLine();
+		String current = "";
+
+		while (!line.equals("Home")) {
+			if (line.equals("back")) {
+				if (!browser.isEmpty()) {
+					current = browser.pop();
+				} else {
+					System.out.println("no previous URLs");
+					line = console.nextLine();
+					continue;
+				}
+			}else{
+				if(!current.equals("")){
+					browser.push(current);
+				}
+				current = line;
+			}
+			System.out.println(current);
+			line = console.nextLine();
+		}
+	}
+}
+
