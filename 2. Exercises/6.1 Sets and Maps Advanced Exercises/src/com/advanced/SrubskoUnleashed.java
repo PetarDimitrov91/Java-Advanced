ee1c62f0a8e38444311321baddeffdad9e8e8c22
PetarDimitrov91 <dimitrovpetar36@gmail.com>
2021-06-01 22:00:20 +0200

    sdasdada

---
+++ 2. Exercises/6.1 Sets and Maps Advanced Exercises/src/com/advanced/SrubskoUnleashed.java
@@ -0,0 +1,69 @@
+package com.advanced;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SrubskoUnleashed {
+    public static void main(String[] args) throws IOException {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+       // String regex = "(?<name>[A-Za-z]+\\s*[A-Za-z]*)\\s+@(?<place>[A-Za-z]+\\s*[A-Za-z]*)\\s+(?<price>\\d+)\\s+(?<count>\\d+)";
+String regex = "(?<name>[A-Z][A-Za-z]+\\s*([A-Z]+[A-Za-z]*)*)\\s+@(?<place>[A-Z][A-Za-z]+\\s*([A-Z]+[A-Za-z]*)*)\\s+(?<price>\\d+)\\s+(?<count>\\d+)";
+        LinkedHashMap<String, LinkedHashMap<String, Integer>> data = new LinkedHashMap<>();
+
+        Pattern pattern = Pattern.compile(regex);
+
+        String command = console.readLine();
+        while (!command.equals("End")) {
+            Matcher matcher = pattern.matcher(command);
+
+            String name = "";
+            String place = "";
+            int ticketPrice = 0;
+            int ticketCount = 0;
+
+            if (matcher.find()) {
+                name = matcher.group("name");
+                place = matcher.group("place");
+                ticketPrice = Integer.parseInt(matcher.group("price"));
+                ticketCount = Integer.parseInt(matcher.group("count"));
+            } else {
+                command = console.readLine();
+                continue;
+            }
+
+            if (!data.containsKey(place)) {
+                data.put(place, new LinkedHashMap<>());
+                data.get(place).put(name, ticketCount * ticketPrice);
+            } else {
+                if (!data.get(place).containsKey(name)) {
+                    data.get(place).put(name,  ticketCount * ticketPrice);
+                } else {
+                    int currentEarnings = data.get(place).get(name);
+                    data.get(place).put(name, currentEarnings + ( ticketCount * ticketPrice));
+                }
+            }
+            command = console.readLine();
+        }
+        //  Lepa Brena @Sunny Beach 25 3500                      Sunny Beach
+        //  Dragana@Belgrade23 3500                              #  Saban Saolic -> 4200000
+        //  Ceca @Sunny Beach 28 3500                            #  Ceca -> 1148000
+        //  Mile Kitic @Sunny Beach 21 3500                      #  Lepa Brena -> 87500
+        //  Ceca @Belgrade 35 3500                               #  Mile Kitic -> 73500
+        //  Ceca @Sunny Beach 70 15000                           Belgrade
+        //  Saban Saolic @Sunny Beach 120 35000                  #  Ceca -> 122500
+        //  End
+
+
+        data.forEach((k,v) -> {
+            System.out.println(k);
+            v.entrySet()
+                    .stream()
+                    .sorted((e1 , e2) -> e2.getValue().compareTo(e1.getValue()))
+                    .forEach(e -> System.out.println("#  " + e.getKey() + " -> " + e.getValue()));
+        });
+    }
+}
