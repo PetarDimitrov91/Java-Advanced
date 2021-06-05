0edcc766d0fe1dd269eba8db0ccc9b0ca7982af0
PetarDimitrov91 <dimitrovpetar36@gmail.com>
2021-06-05 09:26:13 +0200

    some changes

--- 2. Exercises/Exam Preparation/src/Task1/Selling.java
+++ 2. Exercises/Exam Preparation/src/Task1/Selling.java
@@ -10,28 +10,108 @@ public class Selling {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
-        List<List<String>> bakery = new ArrayList<>();
-
         int n = Integer.parseInt(console.nextLine());
-        for (int i = 0; i < n; i++) {
-            bakery.add(i, Arrays.stream(console.nextLine().split("")).collect(Collectors.toList()));
+        String[][] bakery = new String[n][n];
+
+        for (int row = 0; row < n; row++) {
+            bakery[row] = console.nextLine().split("");
         }
 
-        int rowOfS = 0;
-        int colOfS = 0;
+        int rowOfS = -1;
+        int colOfS = -1;
 
-        for (int row = 0; row < bakery.size(); row++) {
-            for (int col = 0; col < bakery.size(); col++) {
-                String currentStr = bakery.get(row).get(col);
+        int firstRowOfPillar = -1;
+        int firstColOfPillar = -1;
+        int secondRowOfPillar = -1;
+        int secondColOfPillar = -1;
+
+        for (int row = 0; row < bakery.length; row++) {
+            for (int col = 0; col < bakery.length; col++) {
+                String currentStr = bakery[row][col];
                 if (currentStr.equals("S")) {
                     rowOfS = row;
                     colOfS = col;
-                    break;
-                }
 
+                }
+                if (currentStr.equals("O")) {
+                    if (firstRowOfPillar == -1) {
+                        firstRowOfPillar = row;
+                        firstColOfPillar = col;
+                    } else {
+                        secondColOfPillar = row;
+                        secondRowOfPillar = col;
+                    }
+                }
             }
         }
+
+        int money = 0;
+        boolean isOut = false;
+
+        while (money < 50) {
+            if (isOut) {
+                System.out.println("Bad news, you are out of the bakery.");
+                break;
+            }
+            String command = console.nextLine();
+            switch (command) {
+                case "right":
+                    colOfS++;
+                    if (isOutOfBackary(bakery, rowOfS, colOfS)) {
+                        isOut = true;
+                        break;
+                    }else if(rowOfS == firstRowOfPillar && colOfS == firstColOfPillar){
+                        bakery[firstRowOfPillar][firstColOfPillar] = "-";
+                        rowOfS = secondRowOfPillar;
+                        colOfS = secondColOfPillar;
+
+                    }
+                    break;
+                case "left":
+                    colOfS--;
+                    if (isOutOfBackary(bakery, rowOfS, colOfS)) {
+                        isOut = true;
+                        break;
+                    }else if(rowOfS == firstRowOfPillar && colOfS == firstColOfPillar){
+                        bakery[firstRowOfPillar][firstColOfPillar] = "-";
+                        rowOfS = secondRowOfPillar;
+                        colOfS = secondColOfPillar;
+
+                    }
+                    break;
+                case "up":
+                    rowOfS--;
+                    if (isOutOfBackary(bakery, rowOfS, colOfS)) {
+                        isOut = true;
+                        break;
+                    }else if(rowOfS == firstRowOfPillar && colOfS == firstColOfPillar){
+                        bakery[firstRowOfPillar][firstColOfPillar] = "-";
+                        rowOfS = secondRowOfPillar;
+                        colOfS = secondColOfPillar;
+
+                    }
+                    break;
+                case "down":
+                    rowOfS++;
+                    if (isOutOfBackary(bakery, rowOfS, colOfS)) {
+                        isOut = true;
+                        break;
+                    }else if(rowOfS == firstRowOfPillar && colOfS == firstColOfPillar){
+                        bakery[firstRowOfPillar][firstColOfPillar] = "-";
+                        rowOfS = secondRowOfPillar;
+                        colOfS = secondColOfPillar;
+
+                    }
+                    break;
+            }
+        }
+
+
         System.out.println();
 
+    }
+
+    private static boolean isOutOfBackary(String[][] bakery, int rowOfS, int colOfS) {
+        return rowOfS < 0 || rowOfS > bakery.length - 1 || colOfS < 0 || colOfS > bakery.length - 1;
     }
 }
--- 2. Exercises/Exam Preparation/src/Task1/test.java
+++ 2. Exercises/Exam Preparation/src/Task1/test.java
@@ -2,20 +2,22 @@ package Task1;
 
 import java.lang.reflect.Array;
 import java.util.ArrayDeque;
+import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        ArrayDeque<Integer> s = new ArrayDeque<>();
-        s.offer(50);
-        s.offer(40);
-        s.offer(30);
-        s.offer(30);
-        s.offer(15);
+        Scanner console =new Scanner(System.in);
+        String[][] arr = new String[6][6];
 
-        int e = s.pollLast();
-        s.offer(e + 3);
+        for (int row = 0; row < arr.length ; row++) {
+            arr[row] = console.nextLine().split("");
+        }
 
-
+        for (int row = 0; row < arr.length ; row++) {
+            for (int col = 0; col <arr[row].length ; col++) {
+                arr[row][col] = "-";
+            }
+        }
 
         System.out.println();
     }
