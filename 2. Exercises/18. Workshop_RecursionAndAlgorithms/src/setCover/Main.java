f5b3d97669048447276fb30541e444cd12ae2031
Petar Dimitrov <79804094+PetarDimitrov91@users.noreply.github.com>
2021-08-18 14:04:03 +0200

    dsdasd

---
+++ 2. Exercises/18. Workshop_RecursionAndAlgorithms/src/setCover/Main.java
@@ -0,0 +1,43 @@
+package setCover;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] elements = reader.readLine().substring(10).split(", ");
+        int[] universe = new int[elements.length];
+        for (int i = 0; i < elements.length; i++) {
+            universe[i] = Integer.parseInt(elements[i]);
+        }
+        int numberOfSets = Integer.parseInt(reader.readLine().substring(16));
+        List<int[]> sets = new ArrayList<>();
+        for (int i = 0; i < numberOfSets; i++) {
+            String[] setElements = reader.readLine().split(", ");
+            int[] set = new int[setElements.length];
+            for (int j = 0; j < setElements.length; j++) {
+                set[j] = Integer.parseInt(setElements[j]);
+            }
+            sets.add(set);
+        }
+        List<int[]> chosenSets = chooseSets(sets, universe);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Sets to take (%d):%n", chosenSets.size()));
+        for (int[] set : chosenSets) {
+            sb.append("{ ");
+            sb.append(Arrays.toString(set).replaceAll("\\[|]", ""));
+            sb.append(" }").append(System.lineSeparator());
+        }
+        System.out.println(sb);
+    }
+
+    public static List<int[]> chooseSets(List<int[]> sets, int[] universe) {
+        // TODO
+        return new ArrayList<>();
+    }
+}
---
+++ 2. Exercises/18. Workshop_RecursionAndAlgorithms/src/sumOfCoins/Main.java
@@ -0,0 +1,31 @@
+package sumOfCoins;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String[] elements = in.nextLine().substring(7).split(", ");
+        int[] coins = new int[elements.length];
+        for (int i = 0; i < coins.length; i++) {
+            coins[i] = Integer.parseInt(elements[i]);
+        }
+
+        int targetSum = Integer.parseInt(in.nextLine().substring(5));
+
+
+        Map<Integer, Integer> usedCoins = chooseCoins(coins, targetSum);
+
+        for (Map.Entry<Integer, Integer> usedCoin : usedCoins.entrySet()) {
+            System.out.println(usedCoin.getKey() + " -> " + usedCoin.getValue());
+        }
+    }
+
+    public static Map<Integer, Integer> chooseCoins(int[] coins, int targetSum) {
+        // TODO
+
+        return new LinkedHashMap<>();
+    }
+}
